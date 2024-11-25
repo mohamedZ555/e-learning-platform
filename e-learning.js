aa311174897b863ef81f9baaf9265937285187f3
mohamed-ashraf <mohamef.ashraf877@gmail.com>
2024-11-26 00:49:32 +0200

    m

--- e-learning.js
+++ e-learning.js
@@ -1,5 +1,9 @@
+let DummyUsersTable ={UserId : 3, Email:'basem@basem.com' , Password:123456};
 const log = document.getElementsByClassName("sign-in")[0];
+const SginIn = document.getElementById('sub')
 const btn = document.getElementsByClassName("login")[0];
+const password = document.getElementById('p')
+const email = document.getElementById('e')
 
 btn.addEventListener('click', () => {
     if (log.style.display === 'none') {
@@ -8,3 +12,18 @@ btn.addEventListener('click', () => {
       log.style.display = 'none';
     }
   });
+SginIn.addEventListener('click', () => {
+  let passwordValue = password.value
+  let emailValue = email.value
+  console.log(passwordValue)
+  console.log(emailValue)
+    if (passwordValue == DummyUsersTable.Password && emailValue == DummyUsersTable.Email) {
+     log.style.display = 'none'
+     alert(`welcome ${DummyUsersTable.UserId}`)
+     return ;
+    }
+    alert('please enter the correct creds')
+    log.style.display = 'block'
+  });
+  localStorage.setItem('token' , JSON.stringify({UserId:3}))
+  let ms = JSON.parse(localStorage.getItem('token'))
