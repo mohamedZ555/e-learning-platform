b230add04195e7f3218c2a0b172a2d09042c82ee
mohamed-ashraf <mohamef.ashraf877@gmail.com>
2024-11-28 01:25:11 +0200

    Register

--- e-learning.js
+++ e-learning.js
@@ -1,29 +1,47 @@
-let DummyUsersTable ={UserId : 3, Email:'basem@basem.com' , Password:123456};
+let DummyUsersTable ={UserId : 3, userName:'basem' , Email:'basem@basem.com' , Password:123456};
 const log = document.getElementsByClassName("sign-in")[0];
-const SginIn = document.getElementById('sub')
+const signIn = document.getElementById('sub')
 const btn = document.getElementsByClassName("login")[0];
 const password = document.getElementById('p')
 const email = document.getElementById('e')
+const logInForm = document.getElementById("logInForm")
+const register = document.getElementById("register")
+const regist = document.getElementById("regist")
+const lom = document.getElementById("lom")
+
+
+regist.addEventListener("click", () =>{
+  if (true) {
+    logInForm.style.display = "none"
+    register.style.display = "block"
+  }
+})
+
+lom.addEventListener("click", () =>{
+  if (true) {
+    logInForm.style.display = "block"
+    register.style.display = "none"
+  }
+})
 
 btn.addEventListener('click', () => {
-    if (log.style.display === 'none') {
-      log.style.display = 'block';
+    if (log.style.display === 'block') {
+      log.style.display='none';
     } else {
-      log.style.display = 'none';
+      log.style.display='block';
     }
   });
-SginIn.addEventListener('click', () => {
+signIn.addEventListener('click', () => {
   let passwordValue = password.value
   let emailValue = email.value
-  console.log(passwordValue)
-  console.log(emailValue)
+
     if (passwordValue == DummyUsersTable.Password && emailValue == DummyUsersTable.Email) {
      log.style.display = 'none'
-     alert(`welcome ${DummyUsersTable.UserId}`)
+     alert(`welcome ${DummyUsersTable.userName}`)
      return ;
     }
     alert('please enter the correct creds')
     log.style.display = 'block'
   });
-  localStorage.setItem('token' , JSON.stringify({UserId:3}))
+  localStorage.setItem('token' , JSON.stringify({userName:'basem'}))
   let ms = JSON.parse(localStorage.getItem('token'))
