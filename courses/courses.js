ca8fb8d0253b6cb3af4dc639b294afb30318b6e9
mohamed-ashraf <mohamef.ashraf877@gmail.com>
2024-12-03 01:34:29 +0200

    home page

--- courses/courses.js
+++ courses/courses.js
@@ -18,7 +18,25 @@ const boxWrapper = document.querySelector('.courses-wrapper');
 ,  description :'learn Chemistry 101 in this course'
           },
             {
-            id:2,
+            id:3,
+            img : "../imgs/istockphoto-1931634876-612x612.jpg"
+            ,title:'Chemistry'
+,  description :'learn Chemistry 101 in this course'
+          },
+            {
+            id:4,
+            img : "../imgs/istockphoto-1931634876-612x612.jpg"
+            ,title:'Chemistry'
+,  description :'learn Chemistry 101 in this course'
+          },
+            {
+            id:5,
+            img : "../imgs/istockphoto-1931634876-612x612.jpg"
+            ,title:'Chemistry'
+,  description :'learn Chemistry 101 in this course'
+          },
+            {
+            id:6,
             img : "../imgs/istockphoto-1931634876-612x612.jpg"
             ,title:'Chemistry'
 ,  description :'learn Chemistry 101 in this course'
@@ -37,7 +55,7 @@ courseItems.forEach(({img , title, description})=>{
 let courseTemplate = `
 <div class="box-container">
             <div class="box-image-container">
-              <img src=${img} alt="" />
+              <img class="image-cont" src=${img} alt="" />
             </div>
             <div>
               <h4>${title}</h4>
