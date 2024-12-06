b89bbfc665e62967c3614f1454a28929fb0b7534
Basem mohamed abuzaid <besoelpop2@gmail.com>
2024-12-06 10:19:53 +0200

    new courses logic

--- courses/courses.js
+++ courses/courses.js
@@ -1,58 +1,46 @@
+const boxWrapper = document.querySelector(".courses-wrapper");
 
- 
+let courseItems = [
+  {
+    id: 1,
+    img: "../imgs/istockphoto-1931634876-612x612.jpg",
+    title: "Mathematics",
+    description: "learn maths 101 in this course",
+  },
+  {
+    id: 2,
+    img: "../imgs/istockphoto-1931634876-612x612.jpg",
+    title: "Chemistry",
+    description: "learn Chemistry 101 in this course",
+  },
+  {
+    id: 3,
+    img: "../imgs/istockphoto-1931634876-612x612.jpg",
+    title: "Chemistry",
+    description: "learn Chemistry 101 in this course",
+  },
+  {
+    id: 4,
+    img: "../imgs/istockphoto-1931634876-612x612.jpg",
+    title: "Chemistry",
+    description: "learn Chemistry 101 in this course",
+  },
+  {
+    id: 5,
+    img: "../imgs/istockphoto-1931634876-612x612.jpg",
+    title: "Chemistry",
+    description: "learn Chemistry 101 in this course",
+  },
+  {
+    id: 6,
+    img: "../imgs/istockphoto-1931634876-612x612.jpg",
+    title: "Chemistry",
+    description: "learn Chemistry 101 in this course",
+  },
+];
 
-
-const boxWrapper = document.querySelector('.courses-wrapper');
-          
-          let courseItems = [
-            {
-            id:1,
-            img : "../imgs/istockphoto-1931634876-612x612.jpg"
-            ,title:'Mathematics'
-,  description :'learn maths 101 in this course'
-          },
-            {
-            id:2,
-            img : "../imgs/istockphoto-1931634876-612x612.jpg"
-            ,title:'Chemistry'
-,  description :'learn Chemistry 101 in this course'
-          },
-            {
-            id:3,
-            img : "../imgs/istockphoto-1931634876-612x612.jpg"
-            ,title:'Chemistry'
-,  description :'learn Chemistry 101 in this course'
-          },
-            {
-            id:4,
-            img : "../imgs/istockphoto-1931634876-612x612.jpg"
-            ,title:'Chemistry'
-,  description :'learn Chemistry 101 in this course'
-          },
-            {
-            id:5,
-            img : "../imgs/istockphoto-1931634876-612x612.jpg"
-            ,title:'Chemistry'
-,  description :'learn Chemistry 101 in this course'
-          },
-            {
-            id:6,
-            img : "../imgs/istockphoto-1931634876-612x612.jpg"
-            ,title:'Chemistry'
-,  description :'learn Chemistry 101 in this course'
-          },
-
-        ]
-
-
-let [item_1,item_2 , ...rest] = courseItems;
-
-
-        
-courseItems.forEach(({img , title, description})=>{
-
-   
-let courseTemplate = `
+courseItems.forEach(({ img, title, description }) => {
+  let courseTemplate = `
 <div class="box-container">
             <div class="box-image-container">
               <img class="image-cont" src=${img} alt="" />
@@ -63,13 +51,5 @@ let courseTemplate = `
             </div>
           </div>
           `;
-          boxWrapper.insertAdjacentHTML('afterbegin',courseTemplate)
-})
-
-
-
-
-
-
-
-
+  boxWrapper.insertAdjacentHTML("afterbegin", courseTemplate);
+});
